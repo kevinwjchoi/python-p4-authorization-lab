06cc6412bd445ce72ab5568d4339f37a127e53ac
Kevin Choi <kevinnwjchoi@gmail.com>
2024-06-18 17:22:20 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -18,6 +18,7 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -84,15 +85,26 @@ class CheckSession(Resource):
         
         return {}, 401
 
+
 class MemberOnlyIndex(Resource):
-    
+
     def get(self):
-        pass
+
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        
+        member_articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in member_articles], 200
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        
+        article = Article.query.filter(Article.id == id).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
